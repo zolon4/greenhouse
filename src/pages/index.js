c38aa4e30eccc9a78fb06d5505b8fdbcb716f3da
Zolon <zolonsk8@gmail.com>
2020-10-28 13:02:57 -0500

    new logo

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { useStaticQuery, graphql, Link } from "gatsby"
+import flag from "../images/flag.svg"
 
 function IndexPage() {
   const data = useStaticQuery(graphql`
@@ -21,10 +22,14 @@ function IndexPage() {
 
   let courses = data.allAirtable.edges
   var item = courses[Math.floor(Math.random() * courses.length)].node.data
-  if (typeof window !== "undefined") {
-    window.location.href = `/${item.path}`
-  }
-  return <div className="h-screen w-screen bg-yellow" />
+  console.log(item)
+  return (
+    <div className="h-screen w-screen bg-green flex items-center justify-center flex-col">
+      <Link className="text-yellow" to={`/${item.path}`}>
+        <img src={flag} className="w-full md:max-w-3xl" />
+      </Link>
+    </div>
+  )
 }
 
 export default IndexPage
